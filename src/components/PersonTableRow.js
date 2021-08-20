eb94cac3d64ac87920d1cf135289e65c07c5e6e6
Bruce Dickey <brucedickey@email.com>
2021-08-20 16:11:57 -0600

    Add handlers for show modal buttons in table rows.

--- src/components/PersonTableRow.js
+++ src/components/PersonTableRow.js
@@ -5,6 +5,16 @@ import './PersonTableRow.css';
 function PersonTableRow(props) {
   const person = props.person;
 
+  const showProfileModal = () => {
+    props.setShowProfile(person.id)
+  }
+  const showUpdateModal = () => {
+    props.setShowUpdate(person.id)
+  }
+  const showDeleteModal = () => {
+    props.setShowDelete(person.id)
+  }
+  
   // The mecallapi API index endpoint returns the email in the username field.
   return (
     <tr>
@@ -13,9 +23,9 @@ function PersonTableRow(props) {
       <td>{person.lname}</td>
       <td>{person.username}</td>
       <td>
-        <Button variant="primary" className="action-btn" title="Display profile" onClick={props.setShowProfile(person.id)} > <Eye />          </Button>
-        <Button variant="primary" className="action-btn" title="Update profile"  onClick={props.setShowUpdate(person.id)}  > <PencilSquare /> </Button>
-        <Button variant="danger"  className="action-btn" title="Delete"          onClick={props.setShowDelete(person.id)}  > <TrashFill />    </Button>
+        <Button variant="primary" className="action-btn" title="Display profile" onClick={showProfileModal} > <Eye />          </Button>
+        <Button variant="primary" className="action-btn" title="Update profile"  onClick={showUpdateModal}  > <PencilSquare /> </Button>
+        <Button variant="danger"  className="action-btn" title="Delete"          onClick={showDeleteModal}  > <TrashFill />    </Button>
       </td>
     </tr>
   );
