4896bdc81b7dd286346140aa4091514879a778ef
Bruce Dickey <brucedickey@email.com>
2021-08-20 16:09:55 -0600

    Change class to className.

--- src/components/AddPersonModal.js
+++ src/components/AddPersonModal.js
@@ -11,17 +11,17 @@ function AddPersonModal(props) {
       </Modal.Header>
       <Modal.Body>
         <form>
-          <div class="mb-3">
-            <label for="add-first-name" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="add-first-name" />
+          <div className="mb-3">
+            <label htmlFor="add-first-name" className="form-label">First Name</label>
+            <input type="text" className="form-control" id="add-first-name" />
           </div>
-          <div class="mb-3">
-            <label for="add-last-name" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="add-last-name" />
+          <div className="mb-3">
+            <label htmlFor="add-last-name" className="form-label">Last name</label>
+            <input type="text" className="form-control" id="add-last-name" />
           </div>                            
-          <div class="mb-3">
-            <label for="add-email" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="add-email" />
+          <div className="mb-3">
+            <label htmlFor="add-email" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="add-email" />
           </div>
         </form>
       </Modal.Body>
